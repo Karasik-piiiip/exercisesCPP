743cb41d63ace59887eb194df969d5fa27337bf1
Kirill Osmolovsky <kirillosm09@gmail.com>
2022-05-09 17:13:00 +1000

    solve Weird Algorythm

--- main.cpp
+++ main.cpp
@@ -28,10 +28,27 @@ void Increasing_Array(){
     std::cout << requares;
 }
 
+void Weird(){
+    long long n;
+    std::cin >> n;
+    std::cout << n << " ";
+    while(n != 1){
+        if (n % 2 == 1)
+        {
+            n = n*3+1;
+            std::cout << n << " ";
+        }
+        else
+        {
+            n=  n/2;
+            std::cout << n << " ";
+        }
+    }
+}
 
 
 int main(){
-    Increasing_Array();
-
+    //Increasing_Array();
+    Weird();
     return 0;
 }
